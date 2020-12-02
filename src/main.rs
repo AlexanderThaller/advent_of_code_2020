80b6f1c1515d82a48ab4e731cec3ae108f5c137b
Alexander Thaller <alexander.thaller@trivago.com>
2020-12-02 11:30:16 +0100

    [general] Warn about more stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#![warn(clippy::pedantic)]
+#![warn(clippy::unwrap_used)]
+#![warn(rust_2018_idioms, unused_lifetimes, missing_debug_implementations)]
+#![forbid(unsafe_code)]
 #![feature(test)]
 #[cfg(test)]
 extern crate test;
